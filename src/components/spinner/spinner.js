9cc68101c786b55611de3175655c99021a345f4c
kira s <kira.totti97@gmail.com>
2019-06-14 12:44:22 +0300

    added loading screen

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './spinner.css'
+
+const Spinner = () => {
+  return (
+    <div className="lds-css ng-scope">
+      <div style={{width:100,height:100}} className="lds-ellipsis">
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Spinner;
